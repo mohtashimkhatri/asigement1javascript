7e513a460ccebb40e143fef9fdf728a55a339b6c
hr645364 <128062621+hr645364@users.noreply.github.com>
2023-05-16 11:30:46 -0700

    asigemente completed

---
+++ app.js
@@ -0,0 +1,406 @@
+// alert("Enter Your First Name");
+// alert("Enter Your Last  Name");
+// alert("Enter Your Email");
+// alert("Enter Your Phone Number");
+// alert("Enter Your Pasword");
+
+// var caseQty = 65;
+
+// caseQty = 144;
+// console.log(caseQty);
+
+// var num = 9;
+// console.log(num)
+
+
+// result = 5 + 7;
+// console.log(result);
+
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// console.log(orderTotal);
+
+// var  num = 5;
+// var a = num + 10;
+// console.log(a);
+
+// var product cost = 3.45;//illegal//
+
+// var myNameIsMohtashim = 56;
+// console.log(myNameIsMohtashim);
+
+// var a = 54;
+// console.log(a)
+
+// var firstName = "john";
+// var lastName = " doe";
+// var fullName = firstName + lastName;
+// console.log(fullName);
+
+// firstName   ==>legale
+// age  ==>legale
+// numberOfStudents ==>legale
+// totalAmount ==>legale
+// _privateVariable ==>legale
+// price ==>legale
+// camelCaseVariable ==>legale
+// myVariable123 ==>legale
+// hasPermission ==>legale
+
+
+// var 51514 = illegale
+// var `3634` = illegale
+// var ==--*7 = illegale
+// var abs-n/3g = illegale
+// var abs gsf = illegale
+
+// var remainder = 10 % 3;
+// console.log(remainder);
+
+// var num = 20 % 6;
+// console.log(num);
+
+// var firstNo = 1000;
+// var lastNo = 2000;
+// var answer = firstNo * lastNo;
+// console.log(answer)
+
+// var a = 10;
+// var b = 5;
+
+// var result = a - b;
+// console.log(result)
+
+// var dividend = 17;
+// var divisor = 4;
+// var remainder = dividend % divisor;
+// console.log(remainder);
+
+// var number1 = 5;
+// var number2 = 7;
+
+// var result = number1 * number2;
+
+// alert("The result of multiplying " + " " + "" + result);
+
+
+// var a = 5;
+// a--;
+// console.log(a)
+
+// var x = 100;
+// // x--;
+// --x;
+// console.log(x)
+
+// var x = 50;
+// var y = x++;
+// this value has 50.
+
+// var y = 50;
+// var z = --y;
+//this value of 50.
+
+// var a = 50;
+// a--;
+// console.log(a)
+
+//  var a = 50;
+//  a++;
+//  console.log(a)
+
+//  var number1 = 5;
+//  number1++;
+//  alert("The result of 1 number add "+ number1);
+
+//  var calculatedNum = 2 + (2 * 6);
+/// the calculate number is 14.
+
+// var calculatedNum = (2 + 2) * 6;
+///the calculate number is 24.
+
+// var calculatedNum = (2 + 2) * (4 + 2);
+//the calculate number is 24.
+
+// var calculatedNum = ((2 + 2) * 4) + 2;
+//the calculate number is 18.
+
+// var a = "2" + "5";
+// console.log(a)
+
+// alert("Hello " + "World!")
+/// the alert massege is display Combine "Hello World!"
+
+// alert("33 " + 3);
+/// the alert massege is display Combine "33 3"
+
+// var a = "Pakistan";
+// var b = "Zindabade"
+// alert(a + " " + b);
+
+// var str = "Hello ";
+// var num =  42;
+// var concatenated = str + num;
+// console.log(concatenated)
+
+// var a = "Hello "
+// var b = "World!"
+// var c = a + b;
+// console.log(c)
+
+// var a = prompt("Enter Your First Name");
+// console.log(a)
+
+// var country = prompt("Country?", "China");
+// console.log(country)
+
+// var yourName = prompt("Enter Your Name");
+// console.log(yourName);
+
+// var Name = prompt("eNTER YOUR NAME", "MOHTASHIM");
+
+// var a = "Mohtashim ";
+// var c =  prompt("Enter Your Name", a );
+// console.log(c)
+
+// var a = "Please Your Name :";
+// var b = "Mohtashim";
+// var c = prompt(a, b);
+// alert("Your Response:" + c) 
+
+// var city =  prompt("Enter Your City Name", "Karachi")
+// if (city === "Karachi") {
+//   console.log("The City of Lights");
+// }else{
+//     console.log("Not found")
+// }
+
+
+//  var name =  prompt("Enter Your  Name", "Mohtashim")
+// if (name === "Mohtashim") {
+//     alert("Yes")
+//   }else{
+//     console.log("This name is not availd")
+//   }
+
+// var city = karachi;
+//  if (city === "Karachi") {
+//    console.log("Karachi");
+//  }else{
+//      console.log("Durst Share Kejea")
+//  }
+
+// var x = 1;
+
+// if (x === 1) {
+//   x = 2; 
+// }
+// console.log(x)
+
+
+//  var city =  prompt("Enter Your City Name", "Karachi")
+//  if (city !=  "Karachi") {
+//    console.log("The City of Lights");
+//  }else{
+//      console.log("Not found")
+//  }
+
+
+//  var city =  prompt("Enter Your City Name", "Karachi")
+//  if (city >=   "Karachi") {
+//    console.log("The City of Lights");
+//  }else{
+//      console.log("Not found")
+//     }
+
+
+// var a = 10;
+
+// if(a != 10){
+//     var b = 5;
+//     console.log(b)
+// }
+// console.log(a)
+
+// var a = 85;
+// if(a == 10){
+//     var b = 5;
+//     console.log(b)
+// }else{
+//     console.log(a)
+// }
+
+// var a = prompt("Enter Your City Name", "karchi")
+// if(a == "karchi"){
+//     console.log("Okay")
+// }else{
+//     console.log("barai karam name durst kare")
+// }
+
+// var variable1 = 10;  
+// var variable2 = 85;   
+
+// if (variable1 <= variable2) {
+//   alert("okkkkk");
+// } else {
+//   alert("ok");
+// }
+
+// var marks = parseInt(prompt("Enter your marks:")); 
+
+// var totalMarks = 100;  // Assuming total marks are 100
+// var percentage = (marks / totalMarks) * 100;
+
+// var grade;
+
+// if (percentage >= 90) {
+//   grade = "A";
+// } else if (percentage >= 80) {
+//   grade = "B";
+// } else if (percentage >= 70) {
+//   grade = "C";
+// } else if (percentage >= 60) {
+//   grade = "D";
+// } else if (percentage >= 50) {
+//   grade = "E";
+// } else {
+//   grade = "F";
+// }
+
+// console.log("Your percentage is: " + percentage);
+// console.log("Your grade is: " + grade);
+
+
+// var a = 10;
+// if(a == 10){
+//     alert("Okay")
+// }else{
+//     alert("please value sahi de")
+// }
+
+// var a = prompt("enter Your City NAme")
+
+// if(a == "karachi"){
+//     alert("ye karachi hr");
+// }else if(a == "lahore"){
+//     alert("ye lahore he")
+// }else{
+//     alert("This person is defferent cities")
+// }
+
+// `var a = 5;
+// var c = 15;
+
+// if(a == 5 && c ==15){
+//     console.log("okay")
+// }else {
+//     console.log("manle hamari bate bhia")
+// }
+
+// var a = prompt("")
+// var b = prompt("")
+// if(a == "imran" || b == "arsalan"){
+//     var prompt = prompt("age")
+//     if(prompt <= 60){
+//         alert("ab sb okay he ab jasakte he ")
+//     }else{
+//         alert("age ziyada hogai he")
+//     }
+// }else{
+//     alert("mazrate")
+// }
+
+// var variable1 = 10;  
+// var variable2 = 5;
+
+// if (variable1 >= variable2) {
+//   alert("okay");
+// }else{
+//     alert("ap pehle wale se bade he is liye mazrte")
+// }
+
+// var password = prompt("Enter your password:");
+
+// if (password !== "") {
+//   if (password.length <= 5) {
+//     alert("pasword 5 digit ka karo ");
+//   } else {
+//     alert("OK");
+//   }
+// }
+
+// var a = 1;
+// var c = "Max"
+// if (a === 1) {
+//     if (c === "Max") {
+//     alert("OK");
+//     }else{
+//         alert("kuch karo bhai")
+//     }
+//     }
+
+// var arr = []
+// console.log(arr)
+
+// var arr = ['Hello']
+// console.log(arr)
+
+// var alphabet = ["h", "i", "j", "k"];
+// alert(alphabet[2]);
+
+
+// var alphabet=["h","i","j","k",]
+// var a =alphabet.length;
+// console.log(a);
+
+// var alphabet=["h","i","j","k",]
+// alphabet.pop()
+// console.log(alphabet);
+
+// var alphabet=["h","i","j","k",]
+// alphabet.push("m")
+// console.log(alphabet);
+
+//  var alphabet=["h","i","j","k",]
+//  alphabet.shift();
+//  console.log(alphabet);
+
+//  var alphabet=["h","i","j","k",]
+//  alphabet.unshift("u", "o", 'm');
+//  console.log(alphabet);
+
+// var myArray = ["apple"];
+// myArray.unshift("banana");
+
+// console.log(myArray[0]); 
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.splice(2, 0, "L");
+
+// console.log(sizes);
+
+// var pets = ["cat", "dog", "rabbit", "hamster"];
+
+// for (var i = 0; i < pets.length; i++) {
+
+// }
+
+// for (var i = 0; i < 10; i++) {
+//     if (i === 1) {
+//       alert("Counter value during the second iteration: " + i);
+   
+//     }
+//   }
+  
+
+// var userNames = ["Alice", "Bob", "Charlie", "David"];
+
+// var firstName = prompt("Enter first name");
+
+// for (var i = 0; i < userNames.length; i++) {
+//   if (userNames[i] === firstName) {
+//     console.log(firstName + " is in the user names array.");
+//   }
+// }
+
